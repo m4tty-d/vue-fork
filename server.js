0ba1c17afeb541098b4b8e5267571065e58c5d2b
Máté Donka <mate.salamon@gmail.com>
2018-12-15 19:50:55 +0100

    server for deployment #3
    enable history mode

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const serveStatic = require('serve-static')
+const secure = require('express-force-https')
 const path = require('path')
 
 const app = express()
+
+app.use(secure)
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.get('*', (res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 const port = process.env.PORT || 8080
-console.log(`Server is listening on ${port}`)
 app.listen(port)
+console.log(`Server is listening on ${port}`)
