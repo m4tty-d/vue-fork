16a8b4c8c1f63a776bda40ed66c0305dd4b2a55e
Máté Donka <mate.salamon@gmail.com>
2018-12-15 20:14:50 +0100

    server for deployment #4

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const serveStatic = require('serve-static')
-const secure = require('express-force-https')
 const path = require('path')
 
 const app = express()
 
-app.use(secure)
 app.use(serveStatic(path.join(__dirname, 'dist')))
-app.get('*', (res) => {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
