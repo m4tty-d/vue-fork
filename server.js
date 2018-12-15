e0bec596aba3687d2e016f382b51ed158b343cec
Máté Donka <mate.salamon@gmail.com>
2018-12-15 19:43:32 +0100

    server for deployment #2

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const path = require('path')
 
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
+console.log(`Server is listening on ${port}`)
 app.listen(port)
